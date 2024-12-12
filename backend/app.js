799d9869bd6a3251b9a652d6c974bb1cf6c4ea92
PriyankaDhuri2501 <149864623+PriyankaDhuri2501@users.noreply.github.com>
2024-12-12 18:16:25 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,20 +5,18 @@ require('dotenv').config();
 
 const app = express();
 
-// Middleware
 app.use(cors());
-app.use(express.json()); // Parse incoming JSON requests
+app.use(express.json()); 
 
 // MongoDB connection
 const mongoURI = process.env.MONGO_URI;
 mongoose.connect(mongoURI)
   .then(() => console.log('Connected to MongoDB successfully!'))
   .catch((err) => {
-    console.error('Error connecting to MongoDB:', err.message);
-    process.exit(1); // Exit the application if MongoDB connection fails
+    console.error('Error connecting to MongoDB');
   });
 
-// Routes
+
 const bookRoutes = require('./routes/bookRoutes');
 app.use('/api/books', bookRoutes);
 
@@ -26,6 +24,6 @@ app.use('/api/books', bookRoutes);
 app.get("/check", () =>
   { "working successfully" })
 
-// Start server
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
