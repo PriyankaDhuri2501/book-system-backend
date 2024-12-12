008dac85bb11bf9d88411131d4c7946fdd710bea
PriyankaDhuri2501 <149864623+PriyankaDhuri2501@users.noreply.github.com>
2024-12-12 18:17:30 +0530

    Update bookRoutes.js

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,18 +1,11 @@
-
-
 const express = require('express');
 const router = express.Router();
 const { getBooks, addBook, deleteBook } = require('../controller/bookController');
 
-// Get all books
+router.get('/', getBooks);
 
-
-router.get('/check', getBooks);
-
-// Add a new book
 router.post('/', addBook);
 
-// Delete a book
 router.delete('/:id', deleteBook);
 
 module.exports = router;
